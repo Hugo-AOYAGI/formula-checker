3a3166f2f4dc175556a77999e3d6604fd86e57ba
Hugo-AOYAGI <aoyagihugo@gmail.com>
2020-07-22 20:17:09 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+
+## FORMULA CHECKER
+
+A Website to check if a formula is correct, for instance it checks whether or not you are adding time and length or vectors and scalars in which case it will give you a warning.
+
+> CURRENTLY A WORK IN PROGRESS, IT DOESN'T WORK AS OF NOW (22/07/2020)
+
+### Link :
+
+http://formula-checker.azurewebsites.net/
+
+(Domain name will come later)
---
+++ index.js
@@ -0,0 +1,35 @@
+$(document).ready( () => {
+
+    var mathFieldSpan = document.getElementById('mathfield');
+
+    var MQ = MathQuill.getInterface(2);
+    var mathField = MQ.MathField(mathFieldSpan, {
+        spaceBehavesLikeTab: true,
+    });
+
+    mathField.latex("\\text{Type your formula here}");
+
+    $(mathFieldSpan).on("focusin", () => {
+        $(mathFieldSpan).addClass("mf-focused");
+        if (mathField.latex() == "\\text{Type your formula here}") {
+            mathField.latex("");
+        }
+    });
+
+    $(mathFieldSpan).on("focusout", () => {
+        if (mathField.latex().replace("", "") == "") {
+            $(mathFieldSpan).removeClass("mf-focused");
+            mathField.latex("\\text{Type your formula here}");
+        }
+    });
+
+    $(".help-page").on("click", () => {
+        $(".help-page").css("display", "none");
+    });
+
+    $(".__help-btn").on("click", () => {
+        $(".help-page").css("display", "unset");
+    });
+
+
+});
